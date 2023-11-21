638bcee4d00359c20541d9163b7a07d0474519d8
EugeniuPeruzzi <jaka1997@live.it>
2023-11-21 14:18:51 +0100

    first commit

---
+++ src/org/lessons/java/pojo/Eventi.java
@@ -0,0 +1,44 @@
+package org.lessons.java.pojo;
+
+public class Eventi {
+	 private String titolo;
+	 private int data;
+	 private int postiTotali;
+	 private int postiPrenotati;
+	 
+	 public Eventi(String titolo, int data, int postiTotali, int postiPrenotati) {
+		 
+	 }
+	 
+	 
+	 
+	 
+	 
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+	public void setPostiTotali(int postiTotali) {
+		this.postiTotali = postiTotali;
+	}
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+	public void setPostiPrenotati(int postiPrenotati) {
+		this.postiPrenotati = postiPrenotati;
+	}
+	 
+	 
+	 
+}
